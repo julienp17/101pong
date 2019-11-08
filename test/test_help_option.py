cac41b5dca97dd5481c163a59358c622309af8e2
Julien Pause <julien.pause@epitech.eu>
2019-11-08 18:44:56 +0400

    Add unit tests for print_usage and print_description

--- test/test_help_option.py
+++ test/test_help_option.py
@@ -9,8 +9,10 @@
 "This file contains unit tests for the help option of the 101pong program"
 
 import sys
-from src.help_option import *
+from io import StringIO
 from unittest import TestCase
+from unittest.mock import patch
+from src.help_option import *
 
 class TestHelpOption(TestCase):
 
@@ -42,3 +44,28 @@ class TestHelpOption(TestCase):
         self.assertTrue(is_help_option(argv))
         argv = ["./101pong", "--help"]
         self.assertTrue(is_help_option(argv))
+
+    def test_print_usage(self):
+
+        expected = "USAGE\n" + "    ./101pong x0 y0 z0 x1 y1 z1 n\n"
+        with patch("sys.stdout", new = StringIO()) as mock_stdout:
+            print_usage()
+            self.assertEqual(mock_stdout.getvalue(), expected)
+
+    def test_print_description(self):
+
+        expected =  "USAGE\n"
+        expected += "    ./101pong x0 y0 z0 x1 y1 z1 n\n"
+        expected += "\n"
+        expected += "DESCRIPTION\n"
+        expected += "    x0  ball abscissa at time t - 1\n"
+        expected += "    y0  ball ordinate at time t - 1\n"
+        expected += "    z0  ball altitude at time t - 1\n"
+        expected += "    x1  ball abscissa at time t\n"
+        expected += "    y1  ball ordinate at time t\n"
+        expected += "    z1  ball altitude at time t\n"
+        expected += "    n   time shift (greater than or equal to zero, "
+        expected += "integer)\n"
+        with patch("sys.stdout", new = StringIO()) as mock_stdout:
+            print_description()
+            self.assertEqual(mock_stdout.getvalue(), expected)
