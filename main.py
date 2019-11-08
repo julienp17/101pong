3d08cce0b8011a66f0ea771a38d83a4b01f2657c
Julien Pause <julien.pause@epitech.eu>
2019-11-08 11:44:55 +0400

    Add ball_coordinates results in main.c and redirect error messages to stderr

--- main.py
+++ main.py
@@ -6,18 +6,19 @@
 ## Main file for 101pong
 ##
 
-from sys import argv, exit
+from sys import argv, stderr, exit
 from print_res import *
 from vector import *
 from constants import *
-from options import *
+from help_option import *
 
 if (is_help_option(argv)):
     print_description()
     exit(MY_EXIT_SUCCESS)
 if (len(argv) != NB_ARGS):
-    print(INVALID_NB_ARGS_MSG)
+    print(INVALID_NB_ARGS_MSG, file = stderr)
     exit(MY_EXIT_FAILURE)
+
 try:
     x0 = float(argv[1])
     y0 = float(argv[2])
@@ -27,22 +28,16 @@ try:
     z1 = float(argv[6])
     n = int(argv[7])
 except ValueError:
-    print(INCORRECT_TYPE_MSG)
+    print(INCORRECT_TYPE_MSG, file = stderr)
     exit(MY_EXIT_FAILURE)
 if (n < 0):
-    print(NEG_N_MSG)
+    print(NEG_N_MSG, file = stderr)
     exit(MY_EXIT_FAILURE)
 
 vector_1 = Vector(x0, y0, z0)
 vector_2 = Vector(x1, y1, z1)
-
 vel_vector = get_velocity_vector(vector_1, vector_2)
+ball_coordinates = get_ball_coordinates(vector_1, vel_vector, n)
 
 print_vel_vector(vel_vector)
-#print("x0 = %.2f" % x0)
-#print("y0 = " + str(y0))
-#print("z0 = " + str(z0))
-#print("x1 = " + str(x1))
-#print("y1 = " + str(y1))
-#print("z1 = " + str(z1))
-#print("n  = " + str(n))
+print_ball_coordinates(ball_coordinates, n)
