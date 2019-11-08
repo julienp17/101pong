0e11ee593324667da56ad6c031c06d65263eee74
Julien Pause <julien.pause@epitech.eu>
2019-11-08 11:46:05 +0400

    Add get_ball_coordinates in vector.py

--- vector.py
+++ vector.py
@@ -23,3 +23,10 @@ def get_velocity_vector(vector_1: Vector, vector_2: Vector):
     velocity_vector.y = vector_2.y - vector_1.y
     velocity_vector.z = vector_2.z - vector_1.z
     return (velocity_vector)
+
+def get_ball_coordinates(vector_1: Vector, vel_vector: Vector, t: int):
+    ball_coordinates = Vector(0, 0, 0)
+    ball_coordinates.x = vel_vector.x * (t + 1) + vector_1.x
+    ball_coordinates.y = vel_vector.y * (t + 1) + vector_1.y
+    ball_coordinates.z = vel_vector.z * (t + 1) + vector_1.z
+    return (ball_coordinates)
