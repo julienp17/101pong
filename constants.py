084ebeac99835273f99b748404e3287282c4c33a
Julien Pause <julien.pause@epitech.eu>
2019-11-08 10:56:28 +0400

    Add constants.py
    This file is equivalent of a header file in C

---
+++ constants.py
@@ -0,0 +1,16 @@
+# -*-coding:Utf-8 -*
+##
+## EPITECH PROJECT, 2019
+## 101pong
+## File description:
+## Contains constants for 101pong
+##
+
+"""This file contains all constants of 101pong."""
+
+MY_EXIT_SUCCESS     = 0
+MY_EXIT_FAILURE     = 84
+NB_ARGS             = 8
+INVALID_NB_ARGS_MSG = "Invalid number of arguments"
+INCORRECT_TYPE_MSG  = "A parameter is not of correct type"
+NEG_N_MSG           = "N must be greater or equal to zero"
