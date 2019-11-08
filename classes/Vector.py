f43bafe6eda8b8a13bef9ef64c9d10827c0c5c66
Julien Pause <julien.pause@epitech.eu>
2019-11-08 13:48:16 +0400

    Move class Vector to classes/Vector.py

---
+++ classes/Vector.py
@@ -0,0 +1,18 @@
+# -*-coding:Utf-8 -*
+##
+## EPITECH PROJECT, 2019
+## 101pong
+## File description:
+## Class Vector
+##
+
+class Vector:
+
+    """This class represents a vector.
+
+    It holds its x, y, and z value."""
+
+    def __init__(self, x:float, y:float, z:float):
+        self.x = x
+        self.y = y
+        self.z = z
