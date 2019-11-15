9981a4c609757edd71261acaf273b0ff36a07c8f
Julien Pause <julien.pause@epitech.eu>
2019-11-15 14:08:29 +0400

    Initiliaze Vector class to x = 0, y = 0, and z = 0 if no parameters are given when contrusting a new class

--- classes/Vector.py
+++ classes/Vector.py
@@ -12,7 +12,7 @@ class Vector:
 
     It holds its x, y, and z value."""
 
-    def __init__(self, x:float, y:float, z:float):
+    def __init__(self, x:float = 0, y:float = 0, z:float = 0):
         self.x = x
         self.y = y
         self.z = z
